5759d86812820c39e357aeec3ff3ab819cf4ec87
Stijn Debrouwere <stijn@stdout.be>
2014-10-01 13:21:37 +0200

    Clean the data. Cross-reference IPs with a recent GeoIP dataset to get country of origin for each request.
    Rename variables to something more sensible.
    Clean up dates and times. (Daniel did the initial work, I cleaned it up a little.)
    Clean and split up uniform resource identifiers into path, extension, querystring et cetera.
    Create logistic variables to identify file types (page, document, other)
    Create factor for status code type.
    Identify country of origin by cross-referencing IPs with the MaxMinds GeoIP Lite database.
    Distinguish mobile from desktop traffic using the user agent.
    Distinguish robot from human traffic using the user agent.
    Distinguish traffic for the blog from other traffic.
    Create a pageviews subset that contains only requests for (HTML, PHP) pages from humans.

---
+++ munge.R
@@ -0,0 +1,85 @@
+library('tools')
+library('httr')
+library('lubridate')
+library('plyr')
+source('./utils.R')
+
+# define global constants
+PAGE_EXTENSIONS <- c('htm', 'html', 'php')
+DOCUMENT_EXTENSIONS <- c('pdf', 'doc', 'docx', 'xls', 'xlsx')
+STATUS_CODE_TYPES <- list(
+  '1' = 'provisional', 
+  '2' = 'success', 
+  '3' = 'redirect', 
+  '4' = 'client error', 
+  '5' = 'server error'
+  )
+
+# load data
+requests <- read.csv("raw/all.logs.train2.csv", header = TRUE)
+
+# rename some columns to better reflect their nature
+requests <- rename(requests, c(
+  "file_name" = "for_uri", 
+  "ip" = "from_ip", 
+  "user_agent" = "from_user_agent"
+  ))
+
+# convert timezones to ISO format
+requests$timezone <- paste('+0', requests$timezone, sep='')
+# merge date, time and timezone and convert into proper
+# datetime objects from characters
+.datetime <- paste(requests$date, requests$time, requests$timezone)
+requests$date_time = dmy_hms(.datetime)
+requests$human_date_time = as.POSIXlt(requests$date_time)
+
+# categorize status codes by type (success, redirect, client error etc.)
+.status_code_types = substr(as.character(requests$status_code), 0, 1)
+requests$status_code_type = factor(.status_code_types, 
+  levels=names(STATUS_CODE_TYPES), labels=STATUS_CODE_TYPES)
+
+# extract file path from requested url, getting rid of querystring arguments etc.
+requests$for_path <- sapply(requests$for_uri, function (uri) parse_url(uri)$path)
+# categorize requests by type
+requests$for_extension <- as.factor(file_ext(requests$for_path))
+requests$for_page <- ifelse(requests$for_extension %in% PAGE_EXTENSIONS, TRUE, FALSE)
+requests$for_document <- ifelse(requests$for_extension %in% DOCUMENT_EXTENSIONS, TRUE, FALSE)
+requests$for_blog <- grepl('/Blog', requests$for_path)
+
+# figure out mobile traffic from user agent
+requests$from_mobile <- grepl('mobile', requests$from_user_agent, ignore.case = TRUE)
+
+# figure out whether the request was made from a known crawler or bot
+requests$from_robot = sapply(requests$from_user_agent, is_robot)
+                             
+# use the IP address to determine the country from which
+# a visit originates (this might take a minute or two)
+requests$from_ip <- as.character(requests$from_ip)
+requests$from_country <- sapply(requests$from_ip, geolocate)
+
+
+# 1. we start on a Thursday, the 20th of June
+# 2. we end on a Saturday, the 24th of August
+# ==> NOTE: difficult to do per-month statistics because
+# the data is incomplete
+# ==> maybe instead, do per day, or per week, or both
+# (and for weeks, start on Monday, end on Sunday!)
+> max(requests$date_time)
+[1] "2013-08-24 04:34:48 UTC"
+> max(requests$human_date_time)
+[1] "2013-08-24 04:34:48 UTC"
+> max(requests$human_date_time)$wday
+[1] 6
+> min(requests$human_date_time)$wday
+[1] 4
+
+
+# pageviews are defined as those requests that are for a 
+# page and that were not made by a robot or crawler
+# (alternative syntax: `requests[requests$for_page & !requests$from_robot, ]`)
+pageviews <- subset(requests, for_page & !from_robot)
+
+# save our munged and annotated log data
+# (see analyze.R for the actual analysis)
+save(requests, file = 'munged/requests.Rdata')
+save(pageviews, file = 'munged/pageviews.Rdata')
---
+++ utils.R
@@ -0,0 +1,34 @@
+# this code includes GeoLite data created by MaxMind, 
+# available from http://www.maxmind.com
+
+.geoip <- read.csv("raw/GeoIPCountryWhois.csv", 
+  col.names=c('ip_from', 'ip_to', 'ip_int_from', 'ip_int_to', 'country_code', 'country'))
+
+# a function that makes it easier to do GeoIP lookups, by 
+# converting a dotted decimal ip into an integer
+ip_to_integer <- function(address) {
+  segments <- strsplit(address, "\\.")
+  octets <- strtoi(unlist(segments))
+  return(
+    16777216 * octets[1] + 
+    65536 * octets[2] +
+    256 * octets[3] +
+    octets[4]
+  )
+}
+
+# this is not terribly efficient, but works well
+# for smaller datasets
+geolocate <- function(ip) {
+  i <- ip_to_integer(ip)
+  match <- subset(.geoip, ip_int_from <= i & ip_int_to >= i)
+  return(match$country)
+}
+
+# determine whether a user_agent represents a robot or a real human being
+KNOWN_ROBOTS <- c('msnbot', 'googlebot', 'java', 'apache', 'crawler')
+
+is_robot <- function(user_agent) {
+  matches <- sapply(KNOWN_ROBOTS, function(robot) grepl(robot, user_agent))
+  return(any(matches))
+}
