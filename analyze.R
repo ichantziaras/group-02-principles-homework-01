a78c8e963b3e176b44591c324e33685444fd9fb1
Daniel Bestard Delgado <Daniel.BestardDelgado@UGent.be>
2014-10-01 13:38:42 +0200

    Add plots for traffic over time.
    Still needs some work with proper label indexing (week starts on Sunday etc.) and fair comparisons (we don't have logs for three full months.) Just to get us started.

--- analyze.R
+++ analyze.R
@@ -9,3 +9,20 @@ top5 <- head(sort(table(pageviews$for_path), decreasing = TRUE))
 
 # documents downloaded
 table(subset(requests, for_document)$for_extension)
+
+# Plot the number of observations per month
+plot(factor((requests$human_date_time$mon), labels=c("June","July", "August")), main="Number Downloads per Month")
+
+# Plot the number of observations per day for JUNE
+# TODO: labels should start on SUNDAY
+plot(factor(requests$human_date_time$wday,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))[requests$human_date_time$mon == 5],main="Number Downloads per Day in May")
+
+# Plot the number of observations per day for JULY
+plot(factor(requests$human_date_time$wday,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))[requests$human_date_time$mon == 6],main="Number Downloads per Day in June")
+
+# Plot the number of observations per day for AUGUST
+plot(factor(requests$human_date_time$wday,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))[requests$human_date_time$mon == 7],main="Number Downloads per Day in July")
+
+# Check tht the sum of observtions of all histograms is 5000
+length(which(requests$human_date_time$mon==5))+length(which(requests$human_date_time$mon==6))+length(which(requests$human_date_time$mon==7))
+
