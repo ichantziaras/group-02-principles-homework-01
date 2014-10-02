e2e03f19ff63e380d8de8255f6c07b655e68c1a9
Stijn Debrouwere <stijn@stdout.be>
2014-10-02 16:48:17 +0200

    Create loader shortcut.

---
+++ load.R
@@ -0,0 +1,8 @@
+# just a file to conveniently load in the data
+# when working in an interactive session
+# 
+# please don't add any other code here -- 
+# use `analyze.R` or `sandbox.R` instead
+
+load('munged/requests.RData')
+load('munged/pageviews.RData')
